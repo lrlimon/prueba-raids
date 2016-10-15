fff3f5abcaaeb2037c641c4bce0f9c605da8be23
lrlimon <lrlimon@gmail.com>
2016-10-15 00:43:53 -0500

    continue with more changes after test time

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20161015024633) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "votes", force: :cascade do |t|
     t.string   "hero"
     t.string   "email"
